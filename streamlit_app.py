b1f8077137c60b8bdb22db0c3bd58323f961754d
Benoit Dourdet <benoit.dourdet@gmail.com>
2024-11-27 16:23:36 +0000

    maj requirements

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,48 @@
 import streamlit as st
 import utils as u
 import pandas as pd
+from google.cloud import bigquery
+
+# BQ authentification
+client = bigquery.Client()
+project = "jo-paris-2024-442810"
+
+# Perform a query.
+
+Dataframes= [
+"athletes"
+]
+Dataset = "dataset_v2"
+
+
+for Dataframe in Dataframes:
+    query = f"SELECT * FROM `jo-paris-2024-442810.{Dataset}.{Dataframe}'"
+    query_job = client.query(query)  # API request
+    rows = query_job.result()  # Waits for query to finish
+    locals()[Dataframe] = pd.read_gbq(query, project_id=project)
+    print(rows)
+
+
+
+
+
+
+
+
+
+
+#Athlete_histo_1(athletes, x = 'country_code',color="country_code", title="Athletes number per country")
+
+
+
+
+
+
+
+
+
+
+
 
 
 st.set_page_config(
@@ -9,3 +51,12 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded",
 )
+
+
+# Streamlit app
+
+st.title("JO Paris 2024")
+st.markdown("#### Titre")
+
+
+
--- utils.py
+++ utils.py
@@ -1,2 +1,6 @@
 def hello():
     return "hello world"
+
+def Athlete_histo_1(athletes, x, color, title):
+  fig1 = px.histogram(athletes, x = x, color=color, title=title)
+  fig1.show()
