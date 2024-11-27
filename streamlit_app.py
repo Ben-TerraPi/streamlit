f4384f1be39c6f11742e6abebbf93e42f1338fd6
Benoit Dourdet <benoit.dourdet@gmail.com>
2024-11-27 22:05:15 +0000

    maj bigquery v2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import utils as u
 import pandas as pd
+import pandas_gbq
 from google.cloud import bigquery
 from google.oauth2 import service_account
+import plotly.express as px
 
 # Create API client.
 credentials = service_account.Credentials.from_service_account_info(
@@ -15,42 +17,22 @@ project = "jo-paris-2024-442810"
 
 # Perform a query.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
+st.cache_data(ttl=600)
 
-Dataframes = [
-"athletes"
-]
+Dataframes = ["athletes"]
 Dataset = "dataset_v2"
 
 for Dataframe in Dataframes:
     query = f"SELECT * FROM `jo-paris-2024-442810.{Dataset}.{Dataframe}'"
     query_job = client.query(query)  # API request
-    rows = query_job.result()  # Waits for query to finish
-    locals()[Dataframe] = pd.read_gbq(query, project_id=project)
-    print(rows)
-
-
-
-
-
-
-
-
+    #rows = query_job.result()  # Waits for query to finish
+    locals()[Dataframe] = pandas_gbq.read_gbq(query, project_id=project)
+    #print(rows)
 
 
 #Athlete_histo_1(athletes, x = 'country_code',color="country_code", title="Athletes number per country")
-
-
-
-
-
-
-
-
-
-
-
-
+fig1 = px.histogram(athletes, x = 'country_code',color="country_code", title="Athletes number per country")
+fig1.show()
 
 st.set_page_config(
     page_title="JO Paris 2024",
@@ -64,6 +46,3 @@ st.set_page_config(
 
 st.title("JO Paris 2024")
 st.markdown("#### Titre")
-
-
-
