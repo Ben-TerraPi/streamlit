2313e055489d3cffc77d3425ded195fffcec296f
Benoit Dourdet <benoit.dourdet@gmail.com>
2024-11-27 23:32:20 +0000

    import bigquery réussi

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,13 @@
 import streamlit as st
 import utils as u
 import pandas as pd
-import pandas_gbq
 from google.cloud import bigquery
 from google.oauth2 import service_account
 import plotly.express as px
 
-# Create API client.
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"]
-)
+# code a utiliser pour re-run l'appli  >>>>>>>>>>>>>> streamlit run streamlit_app.py
 
-# BQ authentification
-client = bigquery.Client(credentials=credentials)
-project = "jo-paris-2024-442810"
-
-# Perform a query.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-st.cache_data(ttl=600)
-
-Dataframes = ["athletes"]
-Dataset = "dataset_v2"
-
-for Dataframe in Dataframes:
-    query = f"SELECT * FROM `jo-paris-2024-442810.{Dataset}.{Dataframe}'"
-    query_job = client.query(query)  # API request
-    #rows = query_job.result()  # Waits for query to finish
-    locals()[Dataframe] = pandas_gbq.read_gbq(query, project_id=project)
-    #print(rows)
-
-
-#Athlete_histo_1(athletes, x = 'country_code',color="country_code", title="Athletes number per country")
-fig1 = px.histogram(athletes, x = 'country_code',color="country_code", title="Athletes number per country")
-fig1.show()
+#>>>>>>>>>>>>>>>>>>>>> Streamlit page
 
 st.set_page_config(
     page_title="JO Paris 2024",
@@ -41,8 +16,47 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
-
-# Streamlit app
+#>>>>>>>>>>>>>>>>>>>>>> Streamlit app
 
 st.title("JO Paris 2024")
 st.markdown("#### Titre")
+
+#>>>>>>>>>>>>>>>>>>>>>> Application : sidebar
+
+with st.sidebar:
+    st.image("./images/logo-paris-2024.png")
+
+#>>>>>>>>>>>>>>>>>>>>>> Create API client.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+
+#>>>>>>>>>>>>>>>>>>>>>> BQ authentification
+client = bigquery.Client(credentials=credentials)
+project = "jo-paris-2024-442810"
+
+#>>>>>>>>>>>>>>>>>>>>>> Dataframes
+Dataframes = ["athletes"]
+Dataset = "dataset_v2"
+
+
+#>>>>>>>>>>>>>>>>>>>>>> functions
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def get_data_from_bigquery(query):
+    query_job = client.query(query)
+    rows = query_job.result()
+    return pd.DataFrame([dict(row) for row in rows])
+
+@st.cache_data(ttl=600)
+def Athlete_histo_1(athletes, x, color, title):
+  fig1 = px.histogram(athletes, x = x, color=color, title=title)
+  fig1.show()
+
+#>>>>>>>>>>>>>>>>>>>>>> Perform a query.
+
+for Dataframe in Dataframes:
+    query = f"SELECT * FROM `jo-paris-2024-442810.{Dataset}.{Dataframe}`"
+    df = get_data_from_bigquery(query)
+    st.write(f"Voici les données pour {Dataframe}:")
+    st.dataframe(df)
