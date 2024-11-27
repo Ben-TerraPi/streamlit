7c50a63df6bc631e0ebf60fa33ce944e6db029e6
Benoit Dourdet <benoit.dourdet@gmail.com>
2024-11-27 16:59:02 +0000

    maj connect bigquery

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,18 +2,25 @@ import streamlit as st
 import utils as u
 import pandas as pd
 from google.cloud import bigquery
+from google.oauth2 import service_account
+
+# Create API client.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
 
 # BQ authentification
-client = bigquery.Client()
+client = bigquery.Client(credentials=credentials)
 project = "jo-paris-2024-442810"
 
 # Perform a query.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
 
-Dataframes= [
+Dataframes = [
 "athletes"
 ]
 Dataset = "dataset_v2"
-
 
 for Dataframe in Dataframes:
     query = f"SELECT * FROM `jo-paris-2024-442810.{Dataset}.{Dataframe}'"
