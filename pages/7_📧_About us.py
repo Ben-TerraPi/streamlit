e0efa7e135b3163dcc5f2e4c5ecb9c41f00332fa
maxmob35 <136576481+maxmob35@users.noreply.github.com>
2025-01-14 13:57:38 +0100

    Setup

--- pages/7_📧_About us.py
+++ pages/7_📧_About us.py
@@ -45,13 +45,9 @@ with st.container():
             
         Maxime Mobailly ([GitHub](https://github.com/maxmob35))
             
-        Matteo Cherief ([GitHub](https://github.com/Matteo-chf))
-            
-        Gautier Martin ([GitHub](https://github.com/Gautier35400)) 
-            
         · December 2024
         
-        Version 1.0
+        Version 2.0
         """)
             
     
